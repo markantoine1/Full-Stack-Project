41f3f1280d8a3c50e297aeeb0fbb20d90b6af185
Mark Antoine <markantoine1718@gmail.com>
2022-11-25 00:38:07 -0300

    add social media

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,8 +9,12 @@ function App() {
             <img src={logo} alt="DSMeta" />
             <h1>DSMeta</h1>
             <p>
-              Desenvolvido por
-              <a href="https://www.linkedin.com/in/mark-antoine/"> M̲a̲r̲k̲ ̲A̲n̲t̲o̲i̲n̲e̲</a>
+              Desenvolvido por Mark Antoine
+              <br/>
+              Demonstração de notificação: <a href="https://prnt.sc/gBLkk40_Yq9c" target={'_blank'}><b>Clique aqui</b></a>
+              <br />
+              <a id='media' href="https://www.linkedin.com/in/mark-antoine/" target={'_blank'}><b>Linkedin</b> </a>
+              <a id='media' href="https://github.com/markantoine1" target={'_blank'}><b>Github</b></a>
             </p>
         </div>
     </header>
